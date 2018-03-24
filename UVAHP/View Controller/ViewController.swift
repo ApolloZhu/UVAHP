88835e4638ebde3e7f853f2eb17b6f27399f55d9
Elizabeth Louie <lizzie@dannys-2013-13-rmbp.local>
2018-03-24 15:32:17 -0400

    AVCaptureTesting(NotWorking )

--- UVAHP/View Controller/ViewController.swift
+++ UVAHP/View Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,9 +15,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let face = CIFaceFeature()
-        if face.hasLeftEyePosition || face.hasRightEyePosition{
-            print("Hello World!")
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        var frontCamera: AVCaptureDevice? = {
+            guard let devices = AVCaptureDevice.devices(for: AVMediaType.video) as? [AVCaptureDevice] else { return nil }
+            return devices.filter { $0.position == .front }.first
+        }()
+        let deviceInput = try AVCaptureDeviceInput(device: frontCamera!)
+        session.beginConfiguration()
+        if session.canAddInput(deviceInput) {
+            session.addInput(deviceInput)
         }
+//        if face.hasLeftEyePosition || face.hasRightEyePosition{
+//            print("Hello World!")
+//        }
     }
 
     override func didReceiveMemoryWarning() {
