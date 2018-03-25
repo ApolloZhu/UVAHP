7dceab3b88c3e9838ed0abfb81dd9be46710e9a3
ApolloZhu <public-apollonian@outlook.com>
2018-03-24 23:35:39 -0400

    Add Statistics Lookup

--- UVAHP/Model/Statistics/FIPS.swift
+++ UVAHP/Model/Statistics/FIPS.swift
@@ -9,28 +9,30 @@
 import Foundation
 import CoreLocation
 
-fileprivate let fipsURL = ""
-
 struct FIPSWrapper: Decodable {
     let County: County
     struct County: Decodable {
         let FIPS: String
     }
+    let State: State
+    struct State: Decodable {
+        let code: String
+    }
 }
 
 extension CoordinatesConvertible {
-    func fetchFIPS(_ process: @escaping (Int?) -> Void) {
+    func fetchFIPS(_ process: @escaping (String?, Int?) -> Void) {
         guard let coord = coordinates
             , let url = URL(string: "https://geo.fcc.gov/api/census/block/find?format=json"
                 + "&latitude=\(coord.lat)"
                 + "&longitude=\(coord.lng)"
-            ) else { return process(nil) }
+            ) else { return process(nil, nil) }
         let task = URLSession.shared.dataTask(with: url) { (data, _, _) in
             guard let data = data
                 , let json = try? JSONDecoder().decode(FIPSWrapper.self, from: data)
                 , let fips = Int(json.County.FIPS)
-                else { return process(nil) }
-            process(fips)
+                else { return process(nil, nil) }
+            process(json.State.code, fips)
         }
         task.resume()
     }
